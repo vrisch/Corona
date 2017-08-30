146290fe77729bb0716dad751ff7e28f723baf8c
Magnus Nilsson <magnus.nilsson@smartrefill.se>
2017-08-30 18:24:41 +0200

    Orbit

--- Sources/Corona.swift
+++ Sources/Corona.swift
@@ -6,79 +6,76 @@
 //  Copyright © 2017 Corona. All rights reserved.
 //
 
+import Orbit
 import UIKit
 
-public struct Bindings {
-    public enum Event {
-        public enum Result {
-            case empty
-            case string(String)
-            case attributedString(NSAttributedString)
-            case index(Int)
-            case indexPath(IndexPath)
-            case range(NSRange)
-        }
-        
-        case actionPerformed(Result)
-        case selectionChanged(Result)
-        case valueChanged(Result)
-
-        @discardableResult
-        public func empty(result: () throws -> Void) throws -> Event {
-            switch self {
-            case .actionPerformed(.empty): try result()
-            case .selectionChanged(.empty): try result()
-            case .valueChanged(.empty): try result()
-            default: break
-            }
-            return self
-        }
-        @discardableResult
-        public func string(result: (String) throws -> Void) throws -> Event {
-            switch self {
-            case let .actionPerformed(.string(value)): try result(value)
-            case let .selectionChanged(.string(value)): try result(value)
-            case let .valueChanged(.string(value)): try result(value)
-            default: break
-            }
-            return self
-        }
-        @discardableResult
-        public func attributedString(result: (NSAttributedString) throws -> Void) throws -> Event {
-            switch self {
-            case let .actionPerformed(.attributedString(value)): try result(value)
-            case let .selectionChanged(.attributedString(value)): try result(value)
-            case let .valueChanged(.attributedString(value)): try result(value)
-            default: break
-            }
-            return self
-        }
-        @discardableResult
-        public func range(result: (NSRange) throws -> Void) throws -> Event {
-            switch self {
-            case let .actionPerformed(.range(value)): try result(value)
-            case let .selectionChanged(.range(value)): try result(value)
-            case let .valueChanged(.range(value)): try result(value)
-            default: break
-            }
-            return self
-        }
-    }
-    public typealias Change = (Event) throws -> Void
-
-    public init() {}
-
-    public mutating func bind(_ barButtonItem: UIBarButtonItem?, change: @escaping Change) {
-        tokens.append(TargetSelector(target: barButtonItem, change: change))
+public enum Event {
+    public enum Result {
+        case empty
+        case string(String)
+        case attributedString(NSAttributedString)
+        case index(Int)
+        case indexPath(IndexPath)
+        case range(NSRange)
     }
     
-    public mutating func bind(_ control: UIControl?, change: @escaping Change) {
-        tokens.append(TargetSelector(target: control, change: change))
-    }
+    case actionPerformed(Result)
+    case selectionChanged(Result)
+    case valueChanged(Result)
     
-    public mutating func bind(_ textView: UITextView?, change: @escaping Change) {
-        tokens.append(TextViewDelegate(target: textView, change: change))
+    @discardableResult
+    public func empty(result: () throws -> Void) throws -> Event {
+        switch self {
+        case .actionPerformed(.empty): try result()
+        case .selectionChanged(.empty): try result()
+        case .valueChanged(.empty): try result()
+        default: break
+        }
+        return self
     }
-    
-    private var tokens : [Any] = []
+    @discardableResult
+    public func string(result: (String) throws -> Void) throws -> Event {
+        switch self {
+        case let .actionPerformed(.string(value)): try result(value)
+        case let .selectionChanged(.string(value)): try result(value)
+        case let .valueChanged(.string(value)): try result(value)
+        default: break
+        }
+        return self
+    }
+    @discardableResult
+    public func attributedString(result: (NSAttributedString) throws -> Void) throws -> Event {
+        switch self {
+        case let .actionPerformed(.attributedString(value)): try result(value)
+        case let .selectionChanged(.attributedString(value)): try result(value)
+        case let .valueChanged(.attributedString(value)): try result(value)
+        default: break
+        }
+        return self
+    }
+    @discardableResult
+    public func range(result: (NSRange) throws -> Void) throws -> Event {
+        switch self {
+        case let .actionPerformed(.range(value)): try result(value)
+        case let .selectionChanged(.range(value)): try result(value)
+        case let .valueChanged(.range(value)): try result(value)
+        default: break
+        }
+        return self
+    }
 }
+
+public typealias Change = (Event) throws -> Void
+
+public func bind(_ barButtonItem: UIBarButtonItem?, change: @escaping Change) -> Disposable {
+    return Disposable(object: TargetSelector(target: barButtonItem, change: change))
+}
+
+public func bind(_ control: UIControl?, change: @escaping Change) -> Disposable {
+    return Disposable(object: TargetSelector(target: control, change: change))
+}
+
+public func bind(_ textView: UITextView?, change: @escaping Change) -> Disposable {
+    return Disposable(object: TextViewDelegate(target: textView, change: change))
+}
+
--- Sources/TextViewDelegate.swift
+++ Sources/TextViewDelegate.swift
@@ -8,34 +8,32 @@
 
 import UIKit
 
-internal extension Bindings {
-    internal class TextViewDelegate: NSObject, UITextViewDelegate {
-        
-        required init(target: UITextView?, change: @escaping Change) {
-            self.target = target
-            self.change = change
-            super.init()
-            target?.delegate = self
-        }
-        
-        deinit {
-            target?.delegate = nil
-        }
-
-        @objc func textViewDidChange(_ textView: UITextView) {
-            if let text = textView.text {
-                try! change(.valueChanged(.string(text)))
-            }
-            if let attributedText = textView.attributedText {
-                try! change(.valueChanged(.attributedString(attributedText)))
-            }
-        }
-        
-        @objc func textViewDidChangeSelection(_ textView: UITextView) {
-            try! change(.selectionChanged(.range(textView.selectedRange)))
-        }
-        
-        private weak var target: UITextView?
-        private let change: Change
+internal class TextViewDelegate: NSObject, UITextViewDelegate {
+    
+    required init(target: UITextView?, change: @escaping Change) {
+        self.target = target
+        self.change = change
+        super.init()
+        target?.delegate = self
     }
+    
+    deinit {
+        target?.delegate = nil
+    }
+    
+    @objc func textViewDidChange(_ textView: UITextView) {
+        if let text = textView.text {
+            try! change(.valueChanged(.string(text)))
+        }
+        if let attributedText = textView.attributedText {
+            try! change(.valueChanged(.attributedString(attributedText)))
+        }
+    }
+    
+    @objc func textViewDidChangeSelection(_ textView: UITextView) {
+        try! change(.selectionChanged(.range(textView.selectedRange)))
+    }
+    
+    private weak var target: UITextView?
+    private let change: Change
 }
